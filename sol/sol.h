b106bd5a62a51854fd0f93f6a3cef0b6a1aacba6
Rochet2 <rochet2@post.com>
2019-03-01 12:03:27 +0200

    Add files via upload

---
+++ sol/sol.h
@@ -0,0 +1,12 @@
+#ifndef LUA_SOL_H
+#define LUA_SOL_H
+
+// I think I'm paranoid
+// Read top of http://sol2.readthedocs.io/en/latest/safety.html
+#ifndef SOL_CHECK_ARGUMENTS
+#define SOL_CHECK_ARGUMENTS true
+#endif
+
+#include "sol.hpp"
+
+#endif
---
+++ sol/solext.h
@@ -0,0 +1,7 @@
+#ifndef LUA_SOLEXT_H
+#define LUA_SOLEXT_H
+
+#include "sol.h"
+#include "stackinterface.h"
+
+#endif
