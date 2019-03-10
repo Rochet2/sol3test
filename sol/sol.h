155ca3575e989f99503b2cde1b2ea046ad35644a
Rochet2 <tqbattlenet@gmail.com>
2019-03-10 10:50:51 +0200

    Update sol.h

--- sol/sol.h
+++ sol/sol.h
@@ -7,6 +7,16 @@
 #define SOL_CHECK_ARGUMENTS true
 #endif
 
+#include "forward.hpp"
+
+#include "wrap64.h"
+int64_t sol_lua_get(sol::types<int64_t>, lua_State* L, int index, sol::stack::record& tracking);
+template <typename Handler> bool sol_lua_check(sol::types<int64_t>, lua_State* L, int index, Handler&& handler, sol::stack::record& tracking);
+int sol_lua_push(lua_State* L, const int64_t& c);
+uint64_t sol_lua_get(sol::types<uint64_t>, lua_State* L, int index, sol::stack::record& tracking);
+template <typename Handler> bool sol_lua_check(sol::types<uint64_t>, lua_State* L, int index, Handler&& handler, sol::stack::record& tracking);
+int sol_lua_push(lua_State* L, const uint64_t& c);
+
 #include "sol.hpp"
 
 #endif
